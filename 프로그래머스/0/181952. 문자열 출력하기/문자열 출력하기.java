715c0ea2cf2b7d5242a2b3666015be566fbf3890
Jiyoung July <31668639+JiJuOh@users.noreply.github.com>
2024-07-21 17:21:04 +0900

    [level 0] Title: 문자열 출력하기, Time: 202.98 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.System;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);  // 세미콜론 추가
+        sc.close();  // Scanner 객체 닫기
+    }
+}
